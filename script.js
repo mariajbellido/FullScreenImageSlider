ae21932c68ff0fc2ef441e7275dd5888574bc4f9
Maria Jesus Bellido <mariajbellido@gmail.com>
2021-02-28 12:42:37 +0000

    adding js

---
+++ script.js
@@ -0,0 +1,57 @@
+let sliderImages = document.querySelectorAll('.slide'); 
+
+let arrowLeft = document.querySelector('#arrow-left');
+
+let arrowRight = document.querySelector('#arrow-right'); 
+
+let current = 0;
+
+// Clear all images 
+function reset() {
+  for(let i = 0; i < sliderImages.length; i++){
+    sliderImages[i].style.display = "none";
+  }
+}
+
+// Starting slide (initializer)
+function startSlide() {
+  reset();
+  // Getting the first image 
+  sliderImages[0].style.display = "block";
+}
+
+// Show previous 
+function slideLeft() {
+  reset();
+  sliderImages[current - 1].style.display = "block";
+  current--;
+}
+
+// Left arrow click 
+
+arrowLeft.addEventListener('click', function(){
+  if(current === 0){
+    current = sliderImages.length;
+  }
+  slideLeft();
+});
+
+// Show next 
+
+function slideRight() {
+  reset();
+  sliderImages[current + 1].style.display = "block";
+  current++;
+}
+
+// Left arrow click 
+
+arrowRight.addEventListener('click', function(){
+  if(current === sliderImages.length - 1){
+    current = -1;
+  }
+  slideRight();
+});
+
+
+startSlide();
